cc796742fa820df6ad2e73e0bb6520b4535aeae0
raulojeda22 <raulojeda10g@gmail.com>
2024-02-27 22:08:45 +0100

    change scanner path

--- parser/build.sh
+++ parser/build.sh
@@ -5,5 +5,7 @@ git clone https://github.com/tree-sitter/tree-sitter-ruby
 git clone https://github.com/tree-sitter/tree-sitter-php
 git clone https://github.com/tree-sitter/tree-sitter-java
 git clone https://github.com/tree-sitter/tree-sitter-c-sharp
-cp -r tree-sitter-php/php/src tree-sitter-php/src 
+cp -r tree-sitter-php/php/src tree-sitter-php/src
+imp="#include \"../common/scanner.h\""
+sed -i "1s/.*/$imp/" tree-sitter-php/src/scanner.c
 python3.10 build.py
